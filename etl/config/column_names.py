55d6bf5418c47a1d523070c8bf6119e20e14af5f
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2021-12-14 14:29:20 -0300

    Project Init

---
+++ etl/config/column_names.py
@@ -0,0 +1,26 @@
+def column_names():
+        """
+        Returns a list of column names for the dataframe
+        """
+        return {
+        'uf junta de cadastro': 'uf_junta_de_cadastro',
+        'número registro siarco': 'numero_registro_siarco',
+        'data início atividades': 'data_inicio_atividades',
+        'data término atividades' : 'data_termino_atividades',
+        'data extinção empresa': 'data_extincao_empresa',
+        'uf endereço empresa': 'uf_endereco_empresa',
+        'município endereço empresa': 'municipio_endereco_empresa',
+        'bairro endereço empresa': 'bairro_endereco_empresa',
+        'porte': 'porte',
+        'natureza': 'natureza',
+        'é filial?': 'e_filial_',
+        'status siarco': 'status_siarco',
+        'situação siarco': 'situacao_siarco',
+        'seção atividade': 'secao_atividade',
+        'código atividade': 'codigo_atividade',
+        'descrição atividade': 'atividade',
+        'cnpj_siarco': 'cnpj_siarco',
+        'razao social siarco': 'razao_social_siarco',
+        'descrição atividade': 'descricao_atividade'
+
+        }
---
+++ etl/config/sectors.py
@@ -0,0 +1,25 @@
+def sectors():
+
+    return {
+            'A':'AGROPECUÁRIA',
+            'B':'INDÚSTRIA',
+            'C':'INDÚSTRIA',
+            'D':'INDÚSTRIA',
+            'E':'SERVIÇOS',
+            'F':'CONSTRUÇÃO',
+            'G':'COMÉRCIO',
+            'H':'SERVIÇOS',
+            'I':'SERVIÇOS',
+            'J':'SERVIÇOS',
+            'K':'SERVIÇOS',
+            'L':'SERVIÇOS',
+            'M':'SERVIÇOS',
+            'N':'SERVIÇOS',
+            'O':'ADMINISTRAÇÃO PÚBLICA',
+            'P':'SERVIÇOS',
+            'Q':'SERVIÇOS',
+            'R':'SERVIÇOS',
+            'S':'SERVIÇOS',
+            'T':'SERVIÇOS',
+            'U':'ORGANISMOS INTERNACIONAIS'
+            }
