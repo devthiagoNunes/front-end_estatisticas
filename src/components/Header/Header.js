035d2adb3e0a5ae31e355da18794d2f82e697ed9
thiago <desenvolvedor.nunes@gmail.com>
2021-12-24 20:53:12 -0300

    adiciona component header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import { Navbar } from "reactstrap";
+import s from './Header.module.scss';
+import Filtros from "../Filtros";
+import './style.css'
+
+export default () => {
+  return (
+    <Navbar className={s.root}>
+      <div className='header'>
+        <h2>PAINEL DE EMPRESAS DO MARANHÃO - Junta Comercial do Maranhão</h2>
+        <img src="http://api.jucema.ma.gov.br/files/1632146487_logo.svg" alt="logo-jucema"/>
+        <svg xmlns="http:www.w3.org/2000/svg" height="28px" viewBox="0 0 24 24" width="28px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" className="menu-mobile" onClick={() => {
+          document.getElementById('content-filtros').style.display = 'block'
+        }}/>
+        </svg>
+      </div>
+    </Navbar>
+  )
+}
