3aa876ee88aac8abe158c096fbac0fc09695a81f
thiago <desenvolvedor.nunes@gmail.com>
2022-01-12 17:31:41 -0300

    adiciona componentes individuais

--- src/pages/dadosEmpresas/index.js
+++ src/pages/dadosEmpresas/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
+import React from 'react'
+import Header from '../../components/Header/Header'
 import Filtros from '../../components/Filtros'
-import Municipio from '../../components/Graficos/municipio';
-import Mes from '../../components/Graficos/mes';
-import Botoes from '../../components/Botoes';
-import GraficoEmpresa from '../../components/Graficos/tipo-empresa';
-import s from './Layout.module.scss';
+import Municipio from '../../components/Graficos/municipio'
+import Mes from "../../components/Graficos/mes"
+import Botoes from '../../components/Botoes'
+import Porte from '../../components/Graficos/tipo-empresa/porte'
+import Setor from '../../components/Graficos/tipo-empresa/setor'
+import s from './Layout.module.scss'
 import './style.css'
 import './styleGlobal.css'
 
-export default ({setState, data, tipo}) => {
+export default ({setState, tipo}) => {
   return (
     <div>
       <div className={s.wrap}>
@@ -20,14 +21,12 @@ export default ({setState, data, tipo}) => {
           <div className='content-data'>
             <div className='content-tipoEmpresa'>
               <div className='tipoEmpresa'>
-                {data.map((obj, key) => {
-                  if(key === 3) return
-                  return <GraficoEmpresa key={key} tipo={tipo} categoria={obj} classificacao={obj.classificacao}/>
-                })}
+                <Porte />
+                <Setor />
               </div>
-              <Municipio tipo={tipo} total={data[3].quantidade} /> 
+              <Municipio /> 
             </div>
-            <Mes tipo={tipo} categoria={data[3]} />
+            <Mes />
           </div>
         </div>
       </div>
