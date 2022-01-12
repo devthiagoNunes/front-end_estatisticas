a7f3fb4276bb298ebc4165cdbffaf3c7d5aff7df
thiago <desenvolvedor.nunes@gmail.com>
2022-01-12 08:53:51 -0300

    adiciona base para redux

---
+++ src/contexts/GlobalContext/contextProvider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useReducer } from 'react'
+import { ContextGlobal } from './context'
+import {estadoGlobal} from './data'
+import { reducer } from './reducer'
+
+export const ContextProvider = ({children}) => {
+  
+  const [state, dispatch] = useReducer(reducer, estadoGlobal)
+
+  return <ContextGlobal.Provider  value={{state, dispatch}}>{children}</ContextGlobal.Provider>
+}
---
+++ src/contexts/GlobalContext/data.js
@@ -0,0 +1,10 @@
+export const estadoGlobal = {
+  empresaAbertas: true,
+  ano: 2021, 
+  setor: '' ,
+  porte: '',
+  natureza: '',
+  descricao_atividade: '',
+  municipio_empresa: '',
+  secao_atividade: '',
+}
