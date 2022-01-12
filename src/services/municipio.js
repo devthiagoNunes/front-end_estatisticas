deefba549d464edb7299ec25bd0de0c6c04df44c
thiago <desenvolvedor.nunes@gmail.com>
2022-01-12 17:30:55 -0300

    adiciona funcao para buscar os dados de municipio

--- src/services/municipio.js
+++ src/services/municipio.js
@@ -15,6 +15,21 @@ export const getAbertasPorMunicipio = async () => {
     .catch(err => console.log(err))
 }
 
+export const getQuantidadeAbertasPorMunicipio = async () => {
+  return await axios({
+    method: 'POST', 
+    url: 'http://179.127.13.245:3000/query/sql', 
+    headers: {
+      'Target-URL': 'http://pinot-broker:8099',
+    },
+      data: { "sql": `select municipio_empresa, count(*) from statistical where inicio_atividades between '2021-01-01' and '2021-12-31' group by municipio_empresa  limit 600000`}
+    })
+    .then(res => {
+      return res.data.resultTable
+    })
+    .catch(err => console.log(err))
+}  
+
 export const getAbertasAnual = async () => {
   return await axios({
     method: 'POST', 
