8007371a347006171ca33da6e049b10d2ce926fd
thiago <desenvolvedor.nunes@gmail.com>
2022-01-13 17:19:20 -0300

    adiciona actions e muda estado global

--- src/contexts/GlobalContext/data.js
+++ src/contexts/GlobalContext/data.js
@@ -1,10 +1,11 @@
+var initial_date = new Date()
 export const estadoGlobal = {
   empresasAbertas: true,
-  ano: 2021, 
   setor: '' ,
   porte: '',
   natureza: '',
   descricao_atividade: '',
   municipio_empresa: '',
   secao_atividade: '',
+  ano: initial_date.getMonth() >= 2 ? initial_date.getFullYear() : initial_date.getFullYear()-1, 
 }
