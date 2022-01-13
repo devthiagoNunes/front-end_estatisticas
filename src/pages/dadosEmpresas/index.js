9d098dc060965fff8e8f30c136f532c7fca9db71
thiago <desenvolvedor.nunes@gmail.com>
2022-01-13 07:38:04 -0300

    adiciona provider de contexto para o componente

--- src/pages/dadosEmpresas/index.js
+++ src/pages/dadosEmpresas/index.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
+import React from 'react'
+import Header from '../../components/Header/Header'
 import Filtros from '../../components/Filtros'
-import Municipio from '../../components/Graficos/municipio';
-import Mes from '../../components/Graficos/mes';
-import Botoes from '../../components/Botoes';
-import GraficoEmpresa from '../../components/Graficos/tipo-empresa';
-import s from './Layout.module.scss';
+import Municipio from '../../components/Graficos/municipio'
+import Mes from "../../components/Graficos/mes"
+import Botoes from '../../components/Botoes'
+import Porte from '../../components/Graficos/tipo-empresa/porte'
+import Setor from '../../components/Graficos/tipo-empresa/setor'
+import Natureza from '../../components/Graficos/tipo-empresa/natureza'
+import { ContextProvider } from '../../contexts/GlobalContext/contextProvider'
+import s from './Layout.module.scss'
 import './style.css'
 import './styleGlobal.css'
 
-export default ({setState, data, tipo}) => {
+export default ({setState, tipo}) => {
   return (
-    <div>
-      <div className={s.wrap}>
-        <Header />
-        <Filtros />
-        <div className='main'>
-          <Botoes setState={setState} tipo={tipo} />
-          <div className='content-data'>
-            <div className='content-tipoEmpresa'>
-              <div className='tipoEmpresa'>
-                {data.map((obj, key) => {
-                  if(key === 3) return
-                  return <GraficoEmpresa key={key} tipo={tipo} categoria={obj} classificacao={obj.classificacao}/>
-                })}
+    <ContextProvider>
+      <div>
+        <div className={s.wrap}>
+          <Header />
+          <Filtros />
+          <div className='main'>
+            <Botoes setState={setState} tipo={tipo} />
+            <div className='content-data'>
+              <div className='content-tipoEmpresa'>
+                <div className='tipoEmpresa'>
+                  <Porte />
+                  <Setor />
+                  <Natureza />
+                </div>
+                <Municipio /> 
               </div>
-              <Municipio tipo={tipo} total={data[3].quantidade} /> 
+              <Mes />
             </div>
-            <Mes tipo={tipo} categoria={data[3]} />
           </div>
         </div>
       </div>
-    </div>
+    </ContextProvider>
   );
 }
