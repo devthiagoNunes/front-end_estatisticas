8db0b20fb4745bc90022e6772c8e4db7f7607845
thiago <desenvolvedor.nunes@gmail.com>
2022-01-13 08:39:15 -0300

    adiciona primeiras action para reducer

---
+++ src/contexts/GlobalContext/actions.js
@@ -0,0 +1,2 @@
+export const MUDAR_ANO = 'MUDAR_ANO'
+export const MUDAR_PORTE = 'MUDAR_PORTE'
--- src/contexts/GlobalContext/contextProvider.js
+++ src/contexts/GlobalContext/contextProvider.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useReducer } from 'react'
 import { ContextGlobal } from './context'
 import { estadoGlobal } from './data'
-import { reducer } from './reducer'
+import * as reducer from '../../reducers/filtros'
 
 export const ContextProvider = ({children}) => {
   
-  const [state, dispatch] = useReducer(reducer, estadoGlobal)
+  const [state, dispatch] = useReducer(reducer.reducerFilter, estadoGlobal)
 
   return <ContextGlobal.Provider  value={{state, dispatch}}>{children}</ContextGlobal.Provider>
 }
