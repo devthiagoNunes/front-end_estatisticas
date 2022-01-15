b4de3c798b5ec92e50cc422d6b87fcd813acd0fe
thiago <desenvolvedor.nunes@gmail.com>
2022-01-15 17:34:30 -0300

    remove useState e adiciona useContext

--- src/components/Botoes/index.js
+++ src/components/Botoes/index.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import {Link} from 'react-router-dom'
+
+import { ContextGlobal } from '../../contexts/GlobalContext/context'
+import * as actions from '../../contexts/GlobalContext/actions'
 import './style.css'
 
-export default ({setState, tipo}) => {
+export default ({tipo}) => {
 
-  let [bgColor, setBackGround] = useState(true)
-  const alterState = (boolean) => setBackGround(boolean)
+  const context = useContext(ContextGlobal)
 
   return(
     <div className="content-buttons">
       <Link to='/estatisticas/empresas-abertas'>
-        <div onClick={() => alterState(true)}>
-          <button onClick={() => setState(true)} style={{
+        <div >
+          <button onClick={() => context.dispatch({type: actions.MUDAR_ESTADO_INICIAL_EMPRESAS, payload: true})} style={{
             color: tipo == 'Abertas' ? 'white' : '#007CC1',
             background: tipo == 'Abertas' ? '#007CC1' : 'white'
           }}>Empresas {tipo == 'Abertas' ? 'Abertas': 'Abertas'}</button>
@@ -19,8 +21,8 @@ export default ({setState, tipo}) => {
       </Link>
       
       <Link to='/estatisticas/empresas-ativas'>
-        <div onClick={() => alterState(false)}>
-          <button onClick={() => setState(false)} style={{
+        <div >
+          <button onClick={() => context.dispatch({type: actions.MUDAR_ESTADO_INICIAL_EMPRESAS, payload: false})} style={{
               color: tipo == 'Ativas' ? 'white' : '#007CC1',
               background: tipo == 'Ativas' ? '#007CC1' : 'white'
             }}>Empresas {tipo == 'Ativas' ? 'Ativas' : 'Ativas'}
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,100 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 
+import { ContextGlobal } from '../../contexts/GlobalContext/context';
 import RenderDatas from "../../pages/dadosEmpresas";
+
 import "./layout.css";
 import "./styleGlobal.css";
 
 export default () => { 
-  const [stateInitial, setStateInitial] = useState(true);
-  const alterState = (boolean) => setStateInitial(boolean);
-
-  const empresasAtivas = [
-    {
-      classificacao: "Porte",
-      empresas: [
-        "Microempresa\nIndividual",
-        "Microempresa",
-        "Pequeno\nPorte",
-        "Outras\nEmpresas",
-      ],
-      quantidade: [157185, 138048, 14022, 37240],
-    },
-    {
-      classificacao: "Setor",
-      empresas: [
-        "Comércio",
-        "Serviços",
-        "Industria",
-        "Construção",
-        "Agropecuária",
-      ],
-      quantidade: [184056, 115875, 23868, 19653, 3043],
-    },
-    {
-      classificacao: "Natureza",
-      empresas: [
-        "EI",
-        "LTDA",
-        "Eireli",
-        "S/A",
-        "Cooperativa",
-        "Empresa Publica",
-        "Mista",
-        "Consorcio",
-        "Autarquia Federal",
-        "Sociedadess",
-        "Associação Privada",
-        "Orgão Publico Poder Executivo",
-        "Estabelecimento de Sociedade\nEstrangeira",
-        "Entidade Sindical",
-      ],
-      quantidade: [
-        257803, 64890, 19095, 3453, 884, 270, 248, 58, 49, 31, 11, 1, 1, 1,
-      ],
-    },
-    {
-      tipo: [
-        "Comércio",
-        "Alojamento e Alimentação",
-        "Industrias de Transformação",
-        "Construção",
-        "Outras Atividades de  Serviços",
-        "Atividades Profissionais,\nCientificas e Tecnicas",
-        "Atividades Administrativas\ne Serviços Complementares",
-        "Transporte, Armazenagem e Correio",
-        "Edcação",
-        "Saude Humana e Serviços Sociais",
-        "Informação e Comunicaçao",
-        "Agricultura, Pecuaria, Produção\nFlorestal, Pesca e Aquicultura",
-        "Artes, Cultura, Esporte e Recepção",
-        "Atividades Financeiras de\nSeguros e Relacionamentos",
-        "Atividades Imobiliarias",
-        "Agua, Esgoto, Atividades de\nGestão de Residuos e Descontaminação",
-        "Serviços Domesticos",
-        "Indusrias Extrativas",
-        "Eletrecidade e Gas",
-        "Administração Publica, Defesa\ne Seguridade Social",
-      ],
-      quantidade: [
-        184056, 24536, 23164, 19653, 18303, 16250, 14514, 12160, 8568, 4340,
-        5035, 3043, 2936, 2542, 2191, 861, 616, 451, 257, 23,
-      ],
-    },
-  ];
+  const context = useContext(ContextGlobal);
 
   return (
     <div>
-      {stateInitial == true && (
+      {context.state.empresasAbertas == true && (
         <RenderDatas
           tipo={"Abertas"}
-          setState={alterState}
         />
       )}
-      {stateInitial != true && (
+      {context.state.empresasAbertas !== true && (
         <RenderDatas
           tipo={"Ativas"}
-          setState={alterState}
-          data={empresasAtivas}
         />
       )}
     </div>
