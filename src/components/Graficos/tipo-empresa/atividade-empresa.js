b9a28ed1fb6408cb11db98e3dcbac56aca678bd3
thiago <desenvolvedor.nunes@gmail.com>
2022-01-19 09:27:59 -0300

    adiciona busca de dados por atividade

---
+++ src/components/Graficos/tipo-empresa/atividade-empresa.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect, useState } from "react"
+import { ContextGlobal } from '../../../contexts/GlobalContext/context'
+import { getDataEmpresasAtivas, getAbertas } from '../../../services/pinot'
+import './style.css'
+
+export default () => {
+
+  const context = useContext(ContextGlobal)
+  const [atividade, setAtividade] = useState([])
+
+  useEffect(() => {
+    const get_abertura_ano = async () => {
+      switch (context.state.empresasAbertas) {
+        case false:
+          const quantidade_ativas =  await getDataEmpresasAtivas('secao_atividade', context)
+          setAtividade(quantidade_ativas.resultTable.rows)
+          return
+      }
+    }
+
+    get_abertura_ano()
+  }, [context])
+
+  return(
+    <div className="content-natureza" style={{
+      marginBottom: context.state.empresasAbertas !== true ? 0 : null,
+      height: context.state.empresasAbertas == true ? '50vh' : '51vh',
+      marginTop: '1rem'
+    }}>
+      <div className="content-dataNatureza" style={{
+        overflowX: context.state.empresasAbertas !== true ? 'hidden' : 'hidden',
+      }}>
+        <p>{`Empresas Ativas Por Atividade `}</p>
+        <div className="content-table-natureza">
+          <div className="tabelas-natureza">
+            <table>
+              <thead>
+                <tr>
+                  <th style={{
+                    textAlign: 'center'
+                  }}>Atividade da Empersa</th>
+                  <th style={{
+                    textAlign: 'center'
+                  }}>Quantidade</th>
+                </tr>
+              </thead>
+              {atividade.map((atividade, index) => (
+                <tbody>
+                  <tr key={index}>
+                    <td style={{
+                      textAlign: "left",
+                      borderRight: '1px solid black'
+                    }}>{atividade[0]}</td>
+                    <td style={{
+                      textAlign: 'center'
+                    }}>{atividade[1]}</td>
+                  </tr> 
+                </tbody>
+              ))}
+              </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
