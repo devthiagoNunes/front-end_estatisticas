bab22042fa20f9c970e7afc264f22c2a29326fec
thiago <desenvolvedor.nunes@gmail.com>
2022-04-01 14:13:33 -0300

    muda style dos filtros selecionados

--- src/contexts/GlobalContext/data.js
+++ src/contexts/GlobalContext/data.js
@@ -8,5 +8,5 @@ export const estadoGlobal = {
   descricao_atividade: '',
   municipio_empresa: '',
   secao_atividade: '',
-  ano: (initial_date.getMonth() >= 1 && initial_date.getDate() >= 3) ? initial_date.getFullYear() : initial_date.getFullYear()-1
+  ano: (initial_date.getMonth() >= 1 && initial_date.getDate() >= 1) ? initial_date.getFullYear() : initial_date.getFullYear()-1
 }
