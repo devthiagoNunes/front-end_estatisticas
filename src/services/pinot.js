a539e273643d981b7e0b3618b2f5c810b07f36d1
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-07-25 10:35:06 -0300

    refactor: remove funcoes nao usadas

--- src/services/pinot.js
+++ src/services/pinot.js
@@ -2,34 +2,6 @@ import axios from 'axios'
 
 const backend_endpoint = 'http://localhost:3333'
 
-export const getCapitalSocial = async (context, endPoint) => {
-  return await axios({
-    method: 'POST',
-    data: {
-      ...context.state
-    },
-    url: `${backend_endpoint}/estatisticas${endPoint}`,
-  })
-  .then(res => {
-    return res.data.values;
-  })
-  .catch(err => err)
-}
-
-export const getFilter = async (context, endPoint) => {
-  return await axios({
-    method: 'POST',
-    data: {
-      ...context.state
-    },
-    url: `${backend_endpoint}/estatisticas${endPoint}`,
-  })
-  .then(res => {
-    return res.data.values;
-  })
-  .catch(err => err)
-}
-
 export const getDatasOfChartsAndFilters = async (contextFilters) => {
   return await axios({
     method: 'POST',
