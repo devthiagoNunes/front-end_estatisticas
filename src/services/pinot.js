79af2d898161e882b06f607397fc106cdb382eea
Carlos Vinicios Martins Rocha <vinicios_martins2011@hotmail.com>
2022-07-27 21:30:21 -0300

    Update pinot.js

--- src/services/pinot.js
+++ src/services/pinot.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const backend_endpoint = 'http://localhost:3333'
+const backend_endpoint = process.env.REACT_APP_BACKEND
 
 export const getDatasOfChartsAndFilters = async (contextFilters) => {
   return await axios({
