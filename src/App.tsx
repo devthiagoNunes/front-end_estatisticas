c9de9517b9542d14dec50a728cd0faf23151f692
thiago <desenvolvedor.nunes@gmail.com>
2022-11-16 19:57:14 -0300

    feat: passando arquivos de js pa tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { ContextProvider } from './contexts/filtersContext/contextProvider'
+import { OpenCompanies } from './pages/openCompanies'
+import { GlobalStyle } from './styles/global-styles'
+import { theme } from './styles/theme'
+
+class App extends React.PureComponent {
+  
+  render() {
+    return (
+        <ContextProvider>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Navigate to="/estatisticas/empresas-abertas" />}/>
+                {/* <Route path="/estatisticas/empresas-abertas" element={<LayoutComponent />}/> */}
+                <Route path="/estatisticas/empresas-abertas" element={<OpenCompanies />}/>
+                {/* <Route path="/estatisticas/empresas-ativas" element={< />}/> */}
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </BrowserRouter>
+            <GlobalStyle />
+          </ThemeProvider>
+        </ContextProvider>
+    );
+  }
+}
+
+export default App
