0a1e3c87ea9984fa363b053e6b5d8d118d4530fe
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 12:40:33 -0300

    feat: continua estilizacao do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,16 +13,18 @@ export const Header = () => {
 
   return (
     <Styled.Container>
-      <h2>PAINEL DE EMPRESAS DO MARANHÃO - Junta Comercial do Maranhão</h2>
-      <img src={Logo} alt="logo-jucema"/>
-      <svg xmlns="http:www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#fff"
-        onClick={() => {
-          setFiltersBlock(!filtersBlock)
-          document.getElementById('content-filtros').style.display = !filtersBlock ? 'block' : 'none'
-        }}
-      ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" className="menu-mobile" 
-      />
-      </svg>
+      <Styled.Content>
+        <h2>PAINEL DE EMPRESAS DO MARANHÃO - Junta Comercial do Maranhão</h2>
+        <img src={Logo} alt="logo-jucema"/>
+        <svg xmlns="http:www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#fff"
+          onClick={() => {
+            setFiltersBlock(!filtersBlock)
+            document.getElementById('content-filtros').style.display = !filtersBlock ? 'block' : 'none'
+          }}
+        ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" className="menu-mobile" 
+        />
+        </svg>
+      </Styled.Content>
     </Styled.Container>
   )
 }
