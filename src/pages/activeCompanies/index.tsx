e98a76522d7dd0b1ac866e4557a3789268ceb927
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 14:25:44 -0300

    feat: cria pagina de empresas ativas

---
+++ src/pages/activeCompanies/index.tsx
@@ -0,0 +1,85 @@
+import { useQuery } from '@tanstack/react-query'
+import { useContext, useEffect, useState } from 'react'
+import { Navigate, useLocation } from 'react-router'
+import { Loading } from '../../components/loading'
+
+import { TemplateChart } from '../../templates/chart'
+import { TemplateLinks } from '../../templates/links'
+import { TemplateTable } from '../../templates/table'
+
+import { getDatasOfChartsAndFilters } from '../../services/pinot'
+import { FilterContext } from '../../contexts/filtersContext/contextProvider'
+
+import * as Styled from './styled'
+import { MapMaranhao } from '../../components/mapMaranhao'
+import { TemplateFilter } from '../../templates/filter'
+import { Header } from '../../components/header'
+import { TreeMapComponente } from '../../components/treeMap'
+
+export const ActiveCompanies = () => {
+  const { pathname } = useLocation()
+  const { state } = useContext(FilterContext)
+  
+  const { data, isLoading, error } = useQuery(['response', state], async () => {
+    const response = await getDatasOfChartsAndFilters(state)
+    return response.data
+  }, {
+    staleTime: 1000 * 10 * 60 // 10 minutes
+  })
+
+  return (
+    <Styled.Container>
+      {isLoading ? <Loading /> : 
+        error ? <Navigate to='/' /> :
+        data && (
+          <Styled.Content>
+            <Header />
+            <Styled.ContentTemplate>
+              <TemplateFilter
+                pathname={pathname}              
+                sectorFilterData={data.filtersData.setor}
+                porteFilterData={data.filtersData.porte}
+                natureFilterData={data.filtersData.natureza}
+                setionFilterData={data.filtersData.secao_atividade}
+                activityFilterData={data.filtersData.descricao_atividade}
+                countyFilterData={data.filtersData.municipio_empresa}
+              />
+  
+              <Styled.StyleContent> 
+                <TemplateLinks />
+  
+                <Styled.ChartsStyle>
+                  <div className='charts-and-tables'>
+                    <Styled.ChartsFirstSection>
+                      <TemplateChart
+                        chartType='Porte'
+                        chartData={data.graphicsData.porte}
+                      />
+                      <TemplateChart
+                        chartType='Setor'
+                        chartData={data.graphicsData.setor}
+                      />                      
+                      {data && <TreeMapComponente treeMapData={data.graphicsData.capitalSocial} />}
+                    </Styled.ChartsFirstSection>
+  
+                    <Styled.MapSection>
+                      <Styled.QuantityTotal>
+                        <p>Total de Empresas {state.empresasAbertas ? 'Abertas' : 'Ativas'}</p>
+                        <p>{data.graphicsData.quantity[0][0].toLocaleString('pt-BR')}</p>
+                      </Styled.QuantityTotal>
+                      <MapMaranhao dataToMap={data.graphicsData.municipio_empresa} />
+                      <TemplateTable 
+                        tableType='Natureza'
+                        tableData={data.graphicsData.natureza}
+                      />
+                    </Styled.MapSection>
+                  </div>
+                </Styled.ChartsStyle> 
+              </Styled.StyleContent>
+            </Styled.ContentTemplate>
+          </Styled.Content>
+        )
+      }
+    </Styled.Container>
+  )
+}
---
+++ src/pages/activeCompanies/styled.ts
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: ${({theme}) => theme.spacings['1.5']};
+  }
+`
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: ${({theme}) => theme.spacings[2]};
+
+  padding-bottom: ${({theme}) => theme.spacings[2]};
+`
+
+export const StyleContent = styled.section`
+  gap: ${({theme}) => theme.spacings['0.5']};
+`
+
+export const ChartsStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({theme}) => theme.spacings[1]};
+  padding: ${({theme}) => theme.spacings['0.75']};
+  border: 1px solid ${({theme}) => theme.colors.blue[400]};
+  
+  .charts-and-tables {
+    display: flex;
+    justify-content: space-evenly;
+    gap: ${({theme}) => theme.spacings[1]};
+  }
+`
+
+export const ChartsFirstSection = styled.section`
+  width: 100%;
+  min-width: 700px;
+  max-width: 750px;
+  max-height: 1050px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${({theme}) => theme.spacings[1]};
+`
+
+export const MapSection = styled.section`
+  width: 100%;
+  max-height: 1050px;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: ${({theme}) => theme.spacings[1]};
+  
+  > div:nth-child(3) {
+    height: 100%;
+    max-height: 348px;
+  }
+
+`
+
+export const QuantityTotal = styled.div`
+  width: 100%;
+  height: 100%;
+  max-height: 150px;
+  min-height: 150px;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3 );
+  border-radius: 15px;
+  overflow: hidden;
+
+  p:nth-child(1) {
+    padding: .5rem 0;
+    
+    color: white;
+    text-align: center;
+    line-height: 2rem;
+    font-size: 1.5rem;
+    font-weight: bold; 
+    background: ${({theme}) => theme.colors.blue[600]};
+  }
+
+  p:nth-child(2) {
+    text-align: center;
+    font-size: 3rem;
+    color: ${({theme}) => theme.font.colors.blue[550]};
+    line-height: 100px;
+  }
+`
+
+export const ContentTemplate = styled(Content)`
+  max-width: 1280px;
+  margin: 0 auto;
+`
