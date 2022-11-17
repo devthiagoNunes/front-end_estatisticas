00a8525dd44c0bfce395bf936804f13a53edde15
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 14:42:24 -0300

    style: reutilizando estilos ja criados e removendo estilos duplicados

--- src/pages/activeCompanies/styled.ts
+++ src/pages/activeCompanies/styled.ts
@@ -1,66 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.main`
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  margin: 0 auto;
+import { Container as PageContainer, Content as PageContent, StyleContent as PageStyleContent, ChartsStyle as PageChartsStyle, ChartsFirstSection as PageChartsFirstSection, MapSection as PageMapSection, QuantityTotal as PageQuantityTotal, ContentTemplate as PageContentTemplate } from '../openCompanies/styled'
 
-  > div {
-    display: flex;
-    flex-direction: column;
-    gap: ${({theme}) => theme.spacings['1.5']};
-  }
-`
+export const Container = styled(PageContainer)``
 
-export const Content = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: ${({theme}) => theme.spacings[2]};
+export const Content = styled(PageContent)``
 
-  padding-bottom: ${({theme}) => theme.spacings[2]};
-`
+export const StyleContent = styled(PageStyleContent)``
 
-export const StyleContent = styled.section`
-  gap: ${({theme}) => theme.spacings['0.5']};
-`
+export const ChartsStyle = styled(PageChartsStyle)``
 
-export const ChartsStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${({theme}) => theme.spacings[1]};
-  padding: ${({theme}) => theme.spacings['0.75']};
-  border: 1px solid ${({theme}) => theme.colors.blue[400]};
-  
-  .charts-and-tables {
-    display: flex;
-    justify-content: space-evenly;
-    gap: ${({theme}) => theme.spacings[1]};
-  }
-`
+export const ChartsFirstSection = styled(PageChartsFirstSection)``
 
-export const ChartsFirstSection = styled.section`
-  width: 100%;
-  min-width: 700px;
-  max-width: 750px;
+export const MapSection = styled(PageMapSection)`
   max-height: 1050px;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${({theme}) => theme.spacings[1]};
-`
-
-export const MapSection = styled.section`
-  width: 100%;
-  max-height: 1050px;
-
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  gap: ${({theme}) => theme.spacings[1]};
-  
   > div:nth-child(3) {
     height: 100%;
     max-height: 348px;
@@ -68,36 +22,6 @@ export const MapSection = styled.section`
 
 `
 
-export const QuantityTotal = styled.div`
-  width: 100%;
-  height: 100%;
-  max-height: 150px;
-  min-height: 150px;
-  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.3 );
-  border-radius: 15px;
-  overflow: hidden;
+export const QuantityTotal = styled(PageQuantityTotal)``
 
-  p:nth-child(1) {
-    padding: .5rem 0;
-    
-    color: white;
-    text-align: center;
-    line-height: 2rem;
-    font-size: 1.5rem;
-    font-weight: bold; 
-    background: ${({theme}) => theme.colors.blue[600]};
-  }
-
-  p:nth-child(2) {
-    text-align: center;
-    font-size: 3rem;
-    color: ${({theme}) => theme.font.colors.blue[550]};
-    line-height: 100px;
-  }
-`
-
-export const ContentTemplate = styled(Content)`
-  max-width: 1280px;
-  margin: 0 auto;
-  padding-top: 2rem;
-`
+export const ContentTemplate = styled(PageContentTemplate)``
