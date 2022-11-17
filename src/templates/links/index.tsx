3f0c3021ba3edace639c6d28a2f50ac9a3e6611b
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 13:21:40 -0300

    feat: mudando estado de empresas ao clicar nos links

--- src/templates/links/index.tsx
+++ src/templates/links/index.tsx
@@ -1,5 +1,10 @@
+import { useContext } from 'react'
 import { useLocation } from 'react-router'
 import { MyLink } from '../../components/myLink'
+import { FilterContext } from '../../contexts/filtersContext/contextProvider'
+
+import * as Actions from '../../contexts/filtersContext/actions'
+
 import * as Styled from './styled'
 
 export type TemplateLinksProps = {
@@ -9,18 +14,27 @@ export type TemplateLinksProps = {
 export const TemplateLinks = () => {
 
   const { pathname } = useLocation()
+  const { dispatch } = useContext(FilterContext)
 
   return (
     <Styled.Container>
       <MyLink 
         pathname={pathname}
         linkText='Empresas Abertas'
+        actionOnClick={() => dispatch({
+          type: Actions.MUDAR_ESTADO_INICIAL_EMPRESAS,
+          payload: true
+        })}
         href='/estatisticas/empresas-abertas'
       />
 
       <MyLink 
         pathname={pathname}
         linkText='Empresas Ativas'
+        actionOnClick={() => dispatch({
+          type: Actions.MUDAR_ESTADO_INICIAL_EMPRESAS,
+          payload: false
+        })}
         href='/estatisticas/empresas-ativas'
       />
     </Styled.Container>
