16ad6ab1c153ff9033b0f23f5947063ebc8c6250
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 15:23:21 -0300

    style: removendo codigo duplicado

--- src/components/mapMaranhao/styled.ts
+++ src/components/mapMaranhao/styled.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { Header as HeaderStyle } from '../chartsHeader/styled'
+
 export const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -23,77 +25,20 @@ export const Container = styled.div`
   }
 `
 
-export const Header = styled.div`
-  display: flex;
-  color: white;
-
-  line-height: 2rem;
-  font-size: 1.5rem;
-  font-weight: bold; 
-  border-top-right-radius: 15px;
-  border-top-left-radius: 15px;
-  text-align: center;
-  background: #007CC1;
-
-  p {
-    width: 95%;
-  }
+export const Header = styled(HeaderStyle)`
+  padding: 0;
 
   .info {
-    max-width: max-content;
-    
-    position: absolute;
     right: 2.1rem;
     top: -.5rem;
-
-    font-size: .6rem;
-    line-height: 2rem;
   }
 
   span {
     .icon-download {
       img {
-        position: absolute;
-        max-width: 35px;
         top: .5rem;
         right: .5rem;
       }
     }
   } 
-
-  @media (max-width: 1024px) {
-    padding: .35rem 0;
-
-    p {
-      font-size: 1.2rem;
-    }
-
-    span {
-      .icon-download {
-        img {
-          position: absolute;
-          top: .45rem;
-          max-width: 30px;
-        }
-      }
-    }
-
-    @media (max-width: 768px) {
-      padding: .25rem 0;
-
-      p {
-        font-size: 1rem;
-      }
-
-      span {
-        .icon-download {
-          img {
-            position: absolute;
-            top: .5rem;
-            max-width: 25px;
-          }
-        }
-      }
-    }
-  }
 `
