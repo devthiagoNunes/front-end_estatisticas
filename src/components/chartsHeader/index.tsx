4b297aea544a40cfadc1ab9c7aff3ea9804fa0a6
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-11-17 15:22:44 -0300

    feat: cria componente de header para os graficos

---
+++ src/components/chartsHeader/index.tsx
@@ -0,0 +1,40 @@
+import { useContext, useState } from 'react'
+import { CSVLink } from 'react-csv'
+
+import DOWNLOADICON from '../../assets/download-icon.svg'
+import { FilterContext } from '../../contexts/filtersContext/contextProvider'
+
+import * as Styled from './styled'
+
+export type ChartsHeaderProps = {
+  textToHeader: string
+  chartData: (string | number)[][]
+  chartType: 'Porte' | 'Setor' | 'Mês' | 'Natureza' | 'Municipio' | 'Capital Social'
+}
+
+export const ChartsHeader = ({ chartType, chartData, textToHeader }: ChartsHeaderProps) => {
+  const { state: { empresasAbertas } } = useContext(FilterContext)
+  const [messageInfo, setMessageInfo] = useState(false);
+
+  const dataToDownload = [
+    [chartType, 'quantidade'],
+    ...chartData
+  ]
+
+  return (
+    <Styled.Header chartType={chartType}>
+      <p>{textToHeader}</p>
+      {messageInfo && <p className='info'>Baixar CSV</p>}
+      <span>
+        <CSVLink data={dataToDownload}
+          filename={`${chartType}-empresa`} 
+          className='icon-download'
+            onMouseOver={() => setMessageInfo(true)}
+            onMouseOut={() => setMessageInfo(false)}
+          > 
+            <img src={DOWNLOADICON} alt="icone de download para arquivo svg" />
+        </CSVLink>
+      </span>
+    </Styled.Header>
+  )
+}
---
+++ src/components/chartsHeader/styled.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+type HeaderProps = {
+  chartType?: string | 'Porte' | 'Setor' | 'Mês'
+}
+
+export const Header = styled.div<HeaderProps>`
+  display: flex;
+  color: white;
+  line-height: 2rem;
+  font-size: 1.5rem;
+  font-weight: bold; 
+
+  border-top-right-radius: 15px;
+  border-top-left-radius: 15px;
+  padding: .5rem 0;
+  text-align: center;
+  background: #007CC1;
+
+  p {
+    width: ${({ chartType }) => chartType !== 'Mês' ? '92%' : '95%'};
+  }
+
+  .info {
+    max-width: max-content;
+    
+    position: absolute;
+    right: ${({ chartType }) => chartType !== 'Mês' ? '3rem' : '4rem'};
+    top: 0rem;
+
+    font-size: .6rem;
+    line-height: 2rem;
+  }
+
+  span {
+    .icon-download {
+      img {
+        position: absolute;
+        max-width: 35px;
+      }
+    }
+  } 
+
+  @media (max-width: 1024px) {
+    padding: .35rem 0;
+
+    p {
+      font-size: 1.2rem;
+    }
+
+    span {
+      .icon-download {
+        img {
+          position: absolute;
+          top: .45rem;
+          max-width: 30px;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      padding: .25rem 0;
+
+      p {
+        font-size: 1rem;
+      }
+
+      span {
+        .icon-download {
+          img {
+            position: absolute;
+            top: .5rem;
+            max-width: 25px;
+          }
+        }
+      }
+    }
+  }
+`
