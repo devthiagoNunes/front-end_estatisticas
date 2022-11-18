aac497763862e25298eb51c7aa654e4ca72679bb
thiago <desenvolvedor.nunes@gmail.com>
2022-11-18 13:10:47 -0300

    feat: addiciona funcionalidade de mostrar ou esconder os filtros na versao mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 
 import Logo from '../../assets/Governo-Maranhao-MA.png'
 
 import * as Styled from './styled'
 
 export type HeaderProps = {
-  
+  filtersVisible: boolean
+  setFiltersVisible: Dispatch<SetStateAction<boolean>>
 }
 
-export const Header = () => {
-  const [filtersBlock, setFiltersBlock] = useState(false)
-
+export const Header = ({ filtersVisible = false, setFiltersVisible }: HeaderProps) => {
   const widthScreen = window.innerWidth
 
   return (
@@ -19,10 +18,7 @@ export const Header = () => {
         <h2>{widthScreen <= 540 ? 'PAINEL DE EMPRESAS DO MARANHÃO - Jucema' : 'PAINEL DE EMPRESAS DO MARANHÃO - Junta Comercial do Maranhão'}</h2>
         <img src={Logo} alt="logo-jucema"/>
         <svg xmlns="http:www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#fff"
-          onClick={() => {
-            setFiltersBlock(!filtersBlock)
-            document.getElementById('content-filtros').style.display = !filtersBlock ? 'block' : 'none'
-          }}
+          onClick={() => setFiltersVisible(!filtersVisible)}
         ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" className="menu-mobile" 
         />
         </svg>
