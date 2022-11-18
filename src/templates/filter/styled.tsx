c3b1f496b8aeb91473abade7ab91f50ddd156628
thiago <desenvolvedor.nunes@gmail.com>
2022-11-18 13:11:44 -0300

    style: ajusta estilo dos filtros para responsividade mobile

--- src/templates/filter/styled.tsx
+++ src/templates/filter/styled.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+type ContainerProps = {
+  filtersVisible: boolean
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   flex-direction: column;
   position: relative;
@@ -17,6 +21,33 @@ export const Container = styled.div`
     font-weight: bold;
     font-size: 1.2rem;
     color: ${({theme}) => theme.colors.blue[600]};
+    
+    @media (max-width: 540px) {
+      position: relative;
+      width: 100%;
+      text-align: center;
+      font-size: ${({theme}) => theme.font.sizes.large};
+    }
+  }
+  
+  @media (max-width: 540px) {
+    width: 100%;
+    height: 100%;
+    left: 100%;
+
+    display: ${({ filtersVisible }) =>  filtersVisible ? 'bloxk' : 'none'};
+    background: white;
+    animation: filterTransition 500ms ease-in-out forwards;
+    transition: 250ms;
+    z-index: 2;
+
+    @keyframes filterTransition {
+      from {
+        left: 100%;
+      } to {
+        left: 0;
+      }
+    }
   }
 `
 
@@ -24,6 +55,10 @@ export const FilterFirstSection = styled.section`
   display: flex;
   gap: ${({theme}) => theme.spacings['0.5']};
   padding-top: ${({theme}) => theme.spacings['0.5']};
+
+  @media (max-width: 540px) {
+    flex-direction: column;
+  }
 `
 
 export const FilterSecondSection = styled(FilterFirstSection)`
